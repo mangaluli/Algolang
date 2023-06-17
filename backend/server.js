d797913eec8765b4efa5f61772c5a730cb727c89
mangaluli <84743239+mangaluli@users.noreply.github.com>
2023-06-17 13:44:46 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
       mongoUrl: process.env.DB,
     }),
     cookie: {
-      secure: true,
+      secure: false,
       httpOnly: true,
       sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24,
